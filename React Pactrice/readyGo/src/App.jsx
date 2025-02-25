1033ef255c2381273a8ffccd2c7522b66650ef7e
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2025-02-26 03:02:46 +0530

    Adding and deleting items in packing list.

--- React Pactrice/readyGo/src/App.jsx
+++ React Pactrice/readyGo/src/App.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 function App() {
+  const [items, setItems] = useState([]);
+
+  function handleItems(item) {
+    setItems((currentItems) => [...currentItems, item]);
+  }
+
+  function handleDeleteItems(id) {
+    setItems((currentItems) => currentItems.filter((item) => item.id !== id));
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form handleItems={handleItems} />
+      <PackingList initialItems={items} handleDeleteItems={handleDeleteItems} />
       <Stats />
     </div>
   );
@@ -13,16 +25,87 @@ function Logo() {
   return <h1>🏝️ ReadyGo 💼</h1>;
 }
 
-function Form() {
+function Form({ handleItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!description) return;
+
+    handleItems({
+      description: description,
+      quantity: quantity,
+      packed: false,
+      id: Date.now(),
+    });
+
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(event) => {
+          setQuantity(Number(event.target.value));
+        }}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        onChange={(event) => {
+          setDescription(event.target.value);
+        }}
+        value={description}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList({ initialItems, handleDeleteItems }) {
+  console.log(initialItems);
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item
+            item={item}
+            key={item.id}
+            handleDeleteItems={handleDeleteItems}
+          />
+        ))}
+      </ul>
     </div>
   );
 }
 
-function PackingList() {
-  return <div className="list">LIST</div>;
+function Item({ item, handleDeleteItems }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {" "}
+        {item.quantity} {item.description}
+      </span>
+      <button
+        onClick={() => {
+          handleDeleteItems(item.id);
+        }}
+      >
+        ❌
+      </button>
+    </li>
+  );
 }
 
 function Stats() {

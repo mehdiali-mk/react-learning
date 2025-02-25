b49d1e66940832528f24d8c49971c63e7569176f
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2025-02-26 03:33:41 +0530

    Statics at the footer is added.

--- React Pactrice/readyGo/src/App.jsx
+++ React Pactrice/readyGo/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
         handleDeleteItems={handleDeleteItems}
         handleToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -128,12 +128,27 @@ function Item({ item, handleDeleteItems, handleToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (items.length === 0)
+    return (
+      <footer className="stats">
+        <em> Start adding some items to your packing list. 🚀</em>
+      </footer>
+    );
+
+  console.log(items);
+
+  const packedItems = items.filter((item) => item.packed).length;
+  const totalItems = items.length;
+  const packedPercentage = Math.round((packedItems / totalItems) * 100);
+
   return (
     <footer className="stats">
       <em>
-        {" "}
-        ` 💼 You have X items on your list, and you already packed X (X%)
+        {packedPercentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : ` 💼 You have ${totalItems} items on your list, and you already packed ${" "}
+          ${packedItems} (${packedPercentage} %)`}
       </em>
     </footer>
   );

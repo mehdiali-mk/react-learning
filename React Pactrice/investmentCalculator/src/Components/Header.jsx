d5665e168c84882162e83c7dc086ad20cd42cb22
Mehdiali Kadiwala <mehdialikadiwala@gmail.com>
2025-04-06 16:25:06 +0530

    Components are seperated from app

---
+++ React Pactrice/investmentCalculator/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import classes from "./Header.module.css";
+
+function Header() {
+  return (
+    <header className={classes.header}>
+      <p>💵</p>
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ React Pactrice/investmentCalculator/src/Components/UserInput.jsx
@@ -0,0 +1,40 @@
+import classes from "./UserInput.module.css";
+
+function UserInput() {
+  return (
+    <form className={classes.form}>
+      <div className={classes.inputGroup}>
+        <p>
+          <label htmlFor="current-savings">Current Savings ($)</label>
+          <input type="number" id="current-savings" />
+        </p>
+        <p>
+          <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+          <input type="number" id="yearly-contribution" />
+        </p>
+      </div>
+      <div className={classes.inputGroup}>
+        <p>
+          <label htmlFor="expected-return">
+            Expected Interest (%, per year)
+          </label>
+          <input type="number" id="expected-return" />
+        </p>
+        <p>
+          <label htmlFor="duration">Investment Duration (years)</label>
+          <input type="number" id="duration" />
+        </p>
+      </div>
+      <p className={classes.actions}>
+        <button type="reset" className={classes.buttonAlt}>
+          Reset
+        </button>
+        <button type="submit" className={classes.button}>
+          Calculate
+        </button>
+      </p>
+    </form>
+  );
+}
+
+export default UserInput;
